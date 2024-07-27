1da2ce93a1bee877c9e42045a9b5b054663d5ecc
Kamlesh Savant <sawantk45@gmail.com>
2024-07-27 12:43:51 +0530

    Added counter functionality

--- xcounter/src/App.js
+++ xcounter/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () =>{
+    setCount(count+1);
+  }
+  const handleDecrement = () =>{
+    setCount(count-1);
+  }
+
   return (
-    <div className="App">
-      
+    <div >
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
